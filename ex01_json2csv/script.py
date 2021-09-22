61cd0b0c1e869db62457a942d11b134cdb80518f
jean PORCHER <jean.porcher@socgen.com>
2021-09-22 12:01:39 +0200

    EX01 done

---
+++ ex01_json2csv/script.py
@@ -0,0 +1,25 @@
+import json
+import csv
+
+src = open("sample.json","r")
+dest = open('./csv_file.csv', 'w',encoding='UTF8')
+
+data = json.loads(src.read())
+writer = csv.writer(dest)
+headers = []
+fields = []
+
+for i in data :
+    for j in data[i] :
+        print(str(data[i][j]))
+        headers.append(str(i+"."+j))
+        fields.append(str(data[i][j]))
+
+
+writer.writerow(headers)
+writer.writerow(fields)
+    
+
+
+dest.close()
+src.close()
